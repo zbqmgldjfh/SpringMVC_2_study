fcc7c276dc265ffd71c32aeddd4308312ed697ce
jiwoo <zbqmgldjfh@gmail.com>
2021-10-17 14:47:23 +0900

    HTTP요청 파라미터(쿼리파라미터 받는법) 1. GET방식으로 쿼리스트링으로 데이터가 온 경우 2. POST방식의 body에 쿼리파라미터가 담겨서 온 경우

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,10 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -73,5 +72,19 @@ public class RequestParamController {
         log.info("username={}, age={}", paramMap.get("username"),
                 paramMap.get("age"));
         return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "OK";
     }
 }
